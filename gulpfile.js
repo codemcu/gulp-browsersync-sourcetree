a55a4af1bd35b7276a602ce1602c4e71e84ebd03
mauricio correa <mauaunot@gmail.com>
2016-03-05 22:44:02 +0100

    Probando watch con sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 /**
- * Created by mauau on 05/03/2016.
+ * Created on 05/03/2016.
  */
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
 gulp.task('styles', function() {
-    gulp.src('src/css/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    return gulp.src('src/css/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('dist/css'))
 });
 
-gulp.task('default', function() {
-    gulp.start(
-        'styles'
-    );
+gulp.task('consologuear', function() {
     console.log('Hello world');
+
 });
+
+gulp.task('watch', function(){
+    gulp.watch('src/css/*.scss', ['styles']);
+});
+
