ae9d899817e0abc82dd355cdf113b4388db6d4e7
mauricio correa <mauaunot@gmail.com>
2016-03-09 23:30:50 +0100

    Actualizacion del package.json y retoques en gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 /**
  * Created on 05/03/2016.
  */
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var watch       = require('gulp-watch');
+var browserSync = require('browser-Sync');
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
+var rename      = require('gulp-rename');
 
 gulp.task('styles', function() {
     return gulp.src('src/css/*.scss')
@@ -11,12 +15,27 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('dist/css'))
 });
 
-gulp.task('consologuear', function() {
-    console.log('Hello world');
+gulp.task('saludar', function(){
+   return gulp.src(['src/js/saludo.js'])
+       .pipe(concat('main.js'))
+       .pipe(gulp.dest('dist/js/'))
+       .pipe(rename('main.min.js'))
+       .pipe(uglify())
+       .pipe(gulp.dest('dist/js/'));
+});
 
+gulp.task('browserSync', function(){
+    return browserSync({
+        server: {
+            baseDir: './'
+        }
+    });
 });
 
 gulp.task('watch', function(){
-    gulp.watch('src/css/*.scss', ['styles']);
-});
+    gulp.watch('src/css/*.scss', ['styles' , browserSync.reload]);
+    gulp.watch('src/js/saludo.js', ['saludar' , browserSync.reload]);
+})
+
+gulp.task('default', ['styles', 'saludar', 'browserSync', 'watch']);
 
